126a71650fe3903e885e1539cc47267795c79cc6
Onimisea <onimisea@gmail.com>
2025-05-15 19:00:12 +0100

    Scaffold

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 font-[family-name:var(--font-geist-sans)]">
+      <p>Welcome to</p>
+      <h1 className="text-[min(10vw,48px)]">Paystup</h1>
+    </div>
+  );
+}
